e368a9916ed0a4901668a69db6071d32c8868654
danieljeswin <danieljeswin@gmail.com>
2020-05-29 21:57:53 -0400

    Reinforce corrected

--- reinforce.py
+++ reinforce.py
@@ -8,157 +8,141 @@ from torch.utils.tensorboard import SummaryWriter
 import time
 import collections
 import gym
+from torch.distributions.categorical import Categorical
 
 class PolicyNetwork(nn.Module):
     def __init__(self, n_states, n_actions):
         super(PolicyNetwork, self).__init__()
         self.fc = nn.Sequential(
-            nn.Linear(n_states, 128),
+            nn.Linear(n_states, 120),
             nn.ReLU(),
-            nn.Linear(128, n_actions)
-            # nn.ReLU(),
-            # nn.Linear(32, n_actions)
+            nn.Linear(120, 84),
+            nn.ReLU(),
+            nn.Linear(84, n_actions)
         )
     
     def forward(self, x):
         output = self.fc(x)
-        return F.softmax(output, dim = 1)
+        return output
 
-class REINFORCE:
+class Reinforce:
     def __init__(self, env_name):
+        
         self.env_name = env_name
         self.env = gym.make(env_name)
+        self.n_actions = self.env.action_space.n
+        self.n_states = self.env.observation_space.shape[0]
         self.device = torch.device('cuda')
 
-        self.n_states = self.env.observation_space.shape[0]
-        self.n_actions = self.env.action_space.n
-
-        self.policy_network = PolicyNetwork(self.n_states, self.n_actions).to(self.device)
-        self.optimizer = optim.Adam(self.policy_network.parameters(), lr = 1e-2)
-
+        self.learning_rate = 7e-4
         self.gamma = 0.99
-        self.total_rewards = []
-        self.best_mean_reward = None
-
+        self.max_episode_length = 500
+        self.total_episodes = 3000
         self.steps = 0
-        self.writer = SummaryWriter(comment = self.env_name + '-myreinforce')
 
-    def get_action(self, state):
-        state = torch.FloatTensor(state).to(self.device)
-        state = torch.unsqueeze(state, dim = 0)
+        self.policy = PolicyNetwork(self.n_states, self.n_actions).to(self.device)
+        self.optimizer = optim.Adam(self.policy.parameters(), lr = self.learning_rate)
+        print(self.policy)
 
-        probs = self.policy_network(state).squeeze()
-        probs = probs.detach().cpu().numpy()
+        self.total_rewards = []
+        self.mean_reward = None
 
-        action = np.random.choice(np.arange(self.n_actions), p = probs)
-
-        return action
-    
     def get_discounted_rewards(self, rewards):
-        cumulative_rewards = 0.0
-        n = rewards.shape[0]
-        for i in range(n - 1, -1, -1):
-            cumulative_rewards += rewards[i]
-            rewards[i] = cumulative_rewards
-            cumulative_rewards *= self.gamma
+        for t in range(self.max_episode_length - 2, -1, -1):
+            rewards[t] += self.gamma * rewards[t + 1]
+        return rewards
 
-        eps = np.finfo(np.float32).eps.item()
-        rewards = (rewards - rewards.mean()) / (rewards.std() + eps)
-        return np.array(rewards)
+    def save_network(self):
+        torch.save(self.policy.state_dict(), self.env_name + "-myreinforce.pth")
 
-    def optimize(self, states, actions, rewards):
-        states = torch.FloatTensor(states).to(self.device)
-        probs = self.policy_network(states)
-        actions = torch.tensor(actions).to(self.device)
+    def load_network(self):
+        self.policy.load_state_dict(torch.load(self.env_name + "-myreinforce.pth"))
 
-        log_probs = torch.log(probs)
-        log_probs = log_probs.gather(1, actions.unsqueeze(-1)).squeeze(-1)
-        rewards = torch.tensor(self.get_discounted_rewards(rewards)).to(self.device)
+    def run_train_episode(self, episode_num):
+        state = self.env.reset()
 
-        loss = -log_probs * rewards
+        ## Initialize the array for logprob, rewards
+        rewards = np.zeros(self.max_episode_length)
+        logprob = torch.zeros((self.max_episode_length)).float().to(self.device)
+        total_reward = 0
+
+        for step in range(self.max_episode_length):
+            self.steps += 1
+
+            ## Logic to select action and get logprob
+            state_t = state.copy()
+            state_t = torch.Tensor(state_t).to(self.device)
+            logits = self.policy(state_t)
+            distribution = Categorical(logits = logits)
+            action = distribution.sample()
+            logprob[step] = distribution.log_prob(action)
+            action = action.item()
+
+            next_state, rewards[step], done, _ = self.env.step(action)
+            total_reward += rewards[step]
+
+            if done: 
+                break
+
+            state = next_state
+        
+        ## Calculate the loss and update the policy
+        discounted_rewards = self.get_discounted_rewards(rewards)
+        discounted_rewards = torch.Tensor(discounted_rewards).to(self.device)
+        loss = -logprob * discounted_rewards
         loss = loss.sum()
 
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
 
-    def run_train_episode(self):
-        state = self.env.reset()
+        # Calculate statistics and display
+        self.total_rewards.append(total_reward)
+        mean_reward = np.mean([self.total_rewards[-100:]])
+        print(f"Done {episode_num} games. Steps {self.steps} Episode reward {total_reward} Mean reward {mean_reward}")
 
-        states = []
-        actions = []
-        rewards = []
-        total_reward = 0.0
-        
-        start_frame = self.steps
-        start_time = time.time()
-
-        while True:
-            self.steps += 1
-            action = self.get_action(state)
-            next_state, reward, done, _ = self.env.step(action)
-            total_reward += reward
-
-            rewards.append(reward)
-            states.append(state)
-            actions.append(action)
-
-            if done:
-                self.total_rewards.append(total_reward)
-                mean_reward = np.mean(self.total_rewards[-100:])
-                if self.best_mean_reward is None or self.best_mean_reward < mean_reward:
-                    torch.save(self.policy_network.state_dict(), self.env_name + '-myreinforce.pth')
-                    if self.best_mean_reward is not None:
-                        print("Best mean reward updated, %0.3f -> %0.3f. model saved" % (self.best_mean_reward, mean_reward))
-                    self.best_mean_reward = mean_reward
-                speed = (self.steps - start_frame) / (time.time() - start_time)
-
-                self.writer.add_scalar("reward", total_reward)
-                self.writer.add_scalar("mean_reward", mean_reward)
-                self.writer.add_scalar("speed", speed)
-                
-                print("%d: Done %d games, mean reward %.3f, speed %.2f f/s" % (self.steps, len(self.total_rewards), mean_reward, 
-                    speed))
-
-                
-                break
-            state = next_state
-        
-        states = np.array(states)
-        actions = np.array(actions)
-        rewards = np.array(rewards)
-
-        self.optimize(states, actions, rewards)
+        if self.mean_reward is None or self.mean_reward < mean_reward:
+            if self.mean_reward is not None:
+                print(f"New best mean {self.mean_reward} -> {mean_reward}. Network saved")
+            self.mean_reward = mean_reward
+            self.save_network()
+        self.writer.add_scalar('Reward', total_reward, self.steps)
+        self.writer.add_scalar('Mean Reward', mean_reward, self.steps)
+            
 
     def train(self):
-        for _ in range(4000):
-            self.run_train_episode()
+        self.writer = SummaryWriter(comment = self.env_name + '-myreinforce')
+        for episode_num in range(self.total_episodes):
+            self.run_train_episode(episode_num)
+        self.writer.close()
+
     
-    def load_network(self):
-        self.policy_network.load_state_dict(torch.load(self.env_name + '-myreinforce.pth'))
-
-    def run_test_episode(self):
-        total_reward = 0.0
-        self.load_network()
+    def run_test_episode(self, episode_num):
         state = self.env.reset()
-
+        total_reward = 0
         while True:
-            action = self.get_action(state)
             self.env.render()
+            # Select action
+            state_t = state.copy()
+            state_t = torch.Tensor(state_t).to(self.device)
+            logits = self.policy(state_t)
+            action = torch.argmax(logits).item()
+
             next_state, reward, done, _ = self.env.step(action)
             total_reward += reward
 
             if done:
-                self.total_rewards.append(total_reward)
-                print("Done %d games, reward %.3f" % (len(self.total_rewards), total_reward))
+                print(f"Episode number : {episode_num}, Reward : {total_reward}")
                 break
             state = next_state
 
     def test(self):
-        for _ in range(20):
-            self.run_test_episode()       
-
+        self.load_network()
+        for episode_num in range(20):
+            self.run_test_episode(episode_num)
+        self.env.close()
 
 if __name__ == '__main__':
-    reinforce_agent = REINFORCE('CartPole-v0')
-    reinforce_agent.train()
+    env_name = 'CartPole-v1'
+    reinforce = Reinforce(env_name)
+    reinforce.test()
